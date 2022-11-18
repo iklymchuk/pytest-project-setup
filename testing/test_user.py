7ee1aa528bcb06b864b8abc5954c8d8dce9c5bfb
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2022-11-18 15:16:51 +0000

    add testing dir

--- testing/test_user.py
+++ testing/test_user.py
@@ -1,4 +1,5 @@
-from  src.user import random_user
+#from ..src.user import random_user
+from src.user import random_user
 
 def test_random_user():
     assert "guest" or "regular" or "admin" in random_user()
