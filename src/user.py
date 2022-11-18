a5bc4a363b7ac744d4d917f61fc6ac1ff4103f58
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2022-11-18 20:53:23 +0000

    fixed lint

--- src/user.py
+++ src/user.py
@@ -6,8 +6,10 @@ def random_user():
     users = ["guest", "regular", "admin"]
     return choices(users)[0]
 
-def greeting(user=random_user()):
-  return f"Hello {user} user!"
 
-if __name__ == '__main__':
-  fire.Fire(greeting)
+def greeting(user=random_user()):
+    return f"Hello {user} user!"
+
+
+if __name__ == "__main__":
+    fire.Fire(greeting)
