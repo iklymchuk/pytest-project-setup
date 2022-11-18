3b13fd5a536542624097f5da9b3dd273c507ad96
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2022-11-18 20:44:55 +0000

    add cli for user function

--- src/user.py
+++ src/user.py
@@ -1,6 +1,13 @@
+import fire
 from random import choices
 
 
 def random_user():
     users = ["guest", "regular", "admin"]
     return choices(users)[0]
+
+def greeting(user=random_user()):
+  return f"Hello {user} user!"
+
+if __name__ == '__main__':
+  fire.Fire(greeting)
