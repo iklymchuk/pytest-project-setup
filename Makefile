ba92e48d027f7fbf9059acd897428ff9b1981e09
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2022-12-10 11:38:33 +0000

    skipped agnostic test for execution

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ install:
 		pip install -r requirements.txt
 
 test:
-	python -m pytest -vv --cov=src testing/
+	python -m pytest -vv --cov=src testing/ -m "skip_agnostic"
 
 parallel-test:
 	python -m pytest -n auto -vv --cov=src testing/ 
--- testing/test_agnostic_api.py
+++ testing/test_agnostic_api.py
@@ -1,19 +1,21 @@
+import pytest
 import requests
 import json
 
 testing_env_companies_url = "http://127.0.0.1:8000/companies/"
 
 
+@pytest.mark.skip_agnostic
 def test_zero_companies():
-    response = requests.get(url=testing_env_companies_url)
+    response = requests.get(url=testing_env_companies_url, timeout=5)
     assert response.status_code == 200
     assert json.loads(response.content) == []
 
 
+@pytest.mark.skip_agnostic
 def test_create_company():
     response = requests.post(
-        url=testing_env_companies_url,
-        json={"name": "test company name"},
+        url=testing_env_companies_url, json={"name": "test company name"}, timeout=5
     )
     assert response.status_code == 201
     response_content = json.loads(response.content)
@@ -23,5 +25,5 @@ def test_create_company():
 
 
 def cleanup_company(company_id: str):
-    response = requests.delete(url=f"testing_env_companies_url{company_id}")
+    response = requests.delete(url=f"testing_env_companies_url{company_id}", timeout=5)
     assert response.status_code == 204
